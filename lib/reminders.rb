ae372ecfdc71d7ac9d4ac3709548e375c31dc5cb
Danny Olson <dbolson@gmail.com>
2013-04-28 16:30:00 -0700

    First client test passing.

--- lib/reminders.rb
+++ lib/reminders.rb
@@ -2,20 +2,20 @@ require 'reminders/version'
 require 'reminders/client'
 
 module Reminders
-  def foo(access_token)
-    @client ||= ::Client.new(access_token)
-  end
+  #def client(access_token)
+    #@client ||= ::Client.new(access_token)
+  #end
 
-  def respond_to?(method_name, include_private=false)
-    client.respond_to?(method_name, include_private) || super
-  end
+  #def respond_to?(method_name, include_private=false)
+    #client.respond_to?(method_name, include_private) || super
+  #end
 
-  private
+  #private
 
-  attr_accessor :client
+  #attr_accessor :client
 
-  def method_missing(method_name, *args, &block)
-    return super unless client.respond_to?(method_name)
-    client.send(method_name, *args, &block)
-  end
+  #def method_missing(method_name, *args, &block)
+    #return super unless client.respond_to?(method_name)
+    #client.send(method_name, *args, &block)
+  #end
 end
--- spec/reminders/client_spec.rb
+++ spec/reminders/client_spec.rb
@@ -3,43 +3,27 @@ require 'reminders'
 require 'reminders/client'
 
 describe Reminders::Client do
-  let(:client) { Reminders::Client }
+  let(:client) { Reminders::Client.new('access_token') }
+  let(:response) { File.read('spec/fixtures/event_lists.json') }
 
   describe '#event_lists' do
     it 'retrieves a json response' do
-      response = %Q{
-        'event_lists': [
-          {
-            'id':3,
-            'name':'name',
-            'created_at':'2013-04-19T13:14:24Z',
-            'updated_at':'2013-04-19T13:14:24Z'
-          },
-          {
-            'id':2,
-            'name':'another name',
-            'created_at':'2013-04-17T15:12:20Z',
-            'updated_at':'2013-04-19T13:06:37Z'
-          }
-        ],
-        'status':200
-      }
       stub_request(:get,
                    'http://localhost:3000/api/v1/event_lists?access_token=access_token')
         .to_return(body: response)
-      expect(client.new('access_token').event_lists).to eq({
+      expect(client.event_lists).to eq({
         'event_lists' => [
           {
-            'id' => 3,
+            'id' => 1,
             'name' => 'name',
-            'created_at' => '2013-04-19T13:14:24Z',
-            'updated_at' => '2013-04-19T13:14:24Z'
+            'created_at' => '2013-04-19T00:00:00Z',
+            'updated_at' => '2013-04-19T00:00:00Z'
           },
           {
             'id' => 2,
             'name' => 'another name',
-            'created_at' => '2013-04-17T15:12:20Z',
-            'updated_at' => '2013-04-19T13:06:37Z'
+            'created_at' => '2013-04-19T00:00:01Z',
+            'updated_at' => '2013-04-19T00:00:01Z'
           }
         ],
         'status' => 200
